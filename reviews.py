57362df3cc334d9ce5f23705eb27d01e86a65662
Oscar <oscar6292@gmail.com>
2019-02-17 14:52:55 +0800

    final ver.

--- reviews.py
+++ reviews.py
@@ -7,8 +7,16 @@ with open('amazonreviews.txt', 'r') as f:
 		if count % 100000 == 0:
 			print(len(data)) #隨時顯示讀取進度
 
-print (len(data))
+print ('檔案讀取完了,總共有: ', len(data), '筆資料')
 
-print (data[136453])
+print (data[0])
 print ('------------------') #視覺分隔線
-print (data[234])
+print (data[1])
+
+#以下算留言平均長度
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('每筆留言平均長度是', sum_len / len(data))
